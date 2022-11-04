b23d5789e72024f6bce0066e5fc4188b823098f7
ValeriSt <46853132+ValeriSt@users.noreply.github.com>
2022-11-04 18:33:51 +0200

    Add files via upload
    SDP-HW1-Solution

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,6 +1,31 @@
 #include "solution.h"
+#include <stack>
+
+using namespace std;
+
+bool arePair(char opening, char closing)
+{
+	if (opening == '(' && closing == ')') return true;
+	else if (opening == '{' && closing == '}') return true;
+	else if (opening == '[' && closing == ']') return true;
+	return false;
+}
 
 bool isCorrect(const char* expression)
 {
-  return false;
+	stack<char> temp;
+
+	for (int i = 0; i < strlen(expression); i++)
+	{
+		if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
+			temp.push(expression[i]);
+		else if (expression[i] == ')' || expression[i] == '}' || expression[i] == ']')
+		{
+			if (temp.empty() || !arePair(temp.top(), expression[i]))
+				return false;
+			else
+				temp.pop();
+		}
+	}
+	return temp.empty() ? true : false;
 }
